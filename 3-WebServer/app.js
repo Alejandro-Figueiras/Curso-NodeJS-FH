1928215a2fdc95436d1ff1d1913ad0487ef11a25
Alejandro <alejandrofigueirasx@gmail.com>
2023-06-17 19:15:08 -0400

    Agregado Handlebars

--- 3-WebServer/app.js
+++ 3-WebServer/app.js
@@ -1,19 +1,32 @@
 const express = require('express')
+const hbs = require('hbs')
+
 const app = express()
 
-// Middleware
+// Handlebars
+app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+// Middleware para elementos estáticos
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: "A",
+        titulo: "Road Trip by TEMPLATED"
+    });
+}) 
+
 app.get('/generic', (req, res) => {
-    res.sendFile(__dirname + '/public/generic.html')
+    res.render('generic')
 })
 
 app.get('/elements', (req, res) => {
-    res.sendFile(__dirname + '/public/elements.html')
+    res.render('elements')
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html')
+    res.render('404')
 })
 
 app.listen(3000)
