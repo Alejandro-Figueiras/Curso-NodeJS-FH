00ee9ab2180f83717a883959c80055de239a0b37
Alejandro <alejandrofigueirasx@gmail.com>
2023-06-17 15:01:32 -0400

    Agregada template de Road Trip by Template

--- 3-WebServer/app.js
+++ 3-WebServer/app.js
@@ -4,6 +4,14 @@ const app = express()
 // Middleware
 app.use(express.static('public'));
 
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
+})
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html')
 })
