b3c27b7c567e878366a2386edb6b09553231c5ea
Alejandro <alejandrofigueirasx@gmail.com>
2023-07-10 20:10:34 -0400

    Agregado roles por db

--- 4-RestServer/routes/usuarios.js
+++ 4-RestServer/routes/usuarios.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { usuariosGet, nuevoUsuario, usuariosPut, usuariosPatch, usuariosDelete } = require("../controllers/usuarios");
 const { check } = require("express-validator");
 const { validarCampos } = require("../middlewares/validarCampos");
+const { isRolValido } = require("../helpers/dbValidators");
 
 const router = Router();
 
@@ -11,7 +12,7 @@ router.post('/new', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password es obligatorio y de mas de 6 letras').isLength({min: 6}),
     check('correo', 'El correo no es valido').isEmail(),
-    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(isRolValido),
     validarCampos
 ], nuevoUsuario)
 router.get('/', usuariosGet)
