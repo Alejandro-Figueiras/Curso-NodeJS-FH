f7b07c084f5fc2e27f9dfe4642ed3ea136e7f99a
Alejandro <alejandrofigueirasx@gmail.com>
2023-07-16 15:23:41 -0400

    Agrega validaciones al usuario del login. TODO: JWT

--- 4-RestServer/controllers/auth.js
+++ 4-RestServer/controllers/auth.js
@@ -1,10 +1,44 @@
 const { request, response } = require("express");
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcryptjs');
 
-const login = (req = request, res = response) => {
-    res.json({
-        msg: "login ok"
-    })
+const login = async(req = request, res = response) => {
 
+    const { correo, password } = req.body;
+
+    try {
+        // Email existe
+        const usuario = await Usuario.findOne({correo});
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password incorrecto'
+            })
+        }
+
+        // Si el usuario esta activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / Password incorrecto'
+            })
+        }
+
+        // Verificar password
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password incorrecto'
+            })
+        }
+
+        // Generar el JWT
+
+        return res.status(200).json({});
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg: 'Error interno'
+        })
+    }
 }
 
 module.exports = {
